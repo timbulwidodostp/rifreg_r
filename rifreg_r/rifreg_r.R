7ad7aa76b1e7b2aef2a379209590ad913e4dc642
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-15 22:33:54 +0700

    Use rifreg With (In) R Software

--- rifreg_r/rifreg_r.R
+++ rifreg_r/rifreg_r.R
@@ -4,8 +4,10 @@
 # Estimate Recentered Influence Function Regression Use rifreg With (In) R Software
 install.packages("rifreg")
 install.packages("base64enc")
+install.packages("gridExtra")
 library("rifreg")
 library("base64enc")
+library("gridExtra")
 rifreg_r = read.csv("https://raw.githubusercontent.com/timbulwidodostp/rifreg_r/main/rifreg_r/rifreg_r.csv",sep = ";")
 # Estimate Recentered Influence Function Regression Use rifreg With (In) R Software
 fm <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
